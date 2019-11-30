8f75a453a9fcd7ededca784959df9e0f382a2b26
hryhorieva <olha.hr.v@gmail.com>
2019-11-30 22:39:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// 1
+// const name = prompt("Enter your name");
+// alert("Hi, " + name + "!");
+
+
+// 2
+// const birthYear = +prompt("Enter your year of birth", 1994);
+// const currentYear = 2019;
+// const age = currentYear - birthYear;
+// console.log("You are " + age + " years old.");
+
+
+// 3
+// const sideLength = +prompt("Enter the length of one side of the square (cm)", 10);
+// const perimeter = sideLength * 4;
+// console.log("The perimeter of the square is " + perimeter + " cm.");
+
+
+// 4
+// const radius = +prompt("Enter the radius of a circle");
+// const numberPi = 3.14;
+// const area = numberPi * radius * radius;
+// console.log("The area of a circle is " + area + " cm.");
+
+
+// 5
+// const distance = +prompt("Enter the distance from city A to city B");
+// const hours = +prompt("In how many hours do you need to reach city B?");
+// const speed = distance / hours;
+// console.log("You need to go with the speed of " + speed + " km per hour to reach city B on time.");
+
+
+// 6
+// const USD = +prompt("Enter the amount in USD");
+// const EUR = 0.91;
+// const EURTotal = USD * EUR;
+// console.log(`${USD}USD is ${EURTotal}EUR`);
+
+
+// 7
+// const Gb = +prompt("Enter the storage capacity of your flesh drive in GB");
+// const Mb = Gb * 1000;
+// const fileSize = 820;
+// const fileAmount = Math.floor(Mb / fileSize);
+// console.log(`${fileAmount} files of 820MB can fit in your flesh drive`);
+
+
+// 8
+// const money = +prompt("How much money do you have?");
+// const chocoPrice = +prompt("How much does 1 chocolate cost?");
+// const chocoNumber = Math.floor(money / chocoPrice);
+// const change = money - (chocoPrice * chocoNumber);
+// console.log(`You can buy ${chocoNumber} chocolates. Your change will be ${change}.`);
+
+
+// 9 
+// const number = +prompt("Enter a 3-digit number", 500);
+// const firstStep = number % 10;
+// const secondStep = number % 100;
+// const thirdStep = Math.floor(secondStep / 10);
+// const fourthStep = Math.floor(number / 100);
+// console.log(firstStep + "" + thirdStep + "" + fourthStep);
+
+
+// 10
+// const sum = +prompt("Enter the sum of the deposit");
+// const interest = 0.05;
+// const month = 2;
+// const yearly = 12;
+// const profit = parseFloat((sum * interest / yearly) * month).toFixed(2);
+// console.log(profit + " is the sum of the 5% interest");
+
+
+
